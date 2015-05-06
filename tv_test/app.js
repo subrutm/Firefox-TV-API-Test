d4e82b2c38c38c54b215e2fab040d6daa693f08c
subrutm <subrutm@gmail.com>
2015-05-06 15:41:37 +0900

    third commit
    videoの再生命令が抜けていたので追加
    ※でも、再生されず

--- tv_test/app.js
+++ tv_test/app.js
@@ -1,3 +1,4 @@
+//デバッグ用HTMLを出力
 function debug(message) {
   var newElement = document.createElement("div");
   var item = "";
@@ -13,27 +14,30 @@ function debug(message) {
 navigator.tv.getTuners().then(function onsuccess(tuners) {
   // Just use the first TV tuner.
   var tuner = tuners[0];
-  alert(tuner);
+  
+  //リンクして再生
   var video = document.getElementById("video-player");
   video.srcObject = tuner.stream;
-
+  video.play();
+/*
   // Set the 'oncurrentsourcechanged' event handler.
   tuner.oncurrentsourcechanged = function oncurrentsourcechanged(event) {
     alert("The current TV source has changed.");
   };
-
+*/
   // Get the supported TV source types for the TV tuner. 
   var sourceTypes = tuner.getSupportedSourceTypes();
   if(sourceTypes.length > 0) {
     debug(sourceTypes);
   }
-
+/*
   // Just use the first TV source type.
   tuner.setCurrentSource(sourceTypes[0]).then(function onsuccess() {
     alert("Succeeded to set the TV source.");
   }, function onerror(error) {
     alert(error);
   });
+*/
 }, function onerror(error) {
   alert(error);
 });
