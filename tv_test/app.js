6ccfd2172470c4fea6170824575368bee04efb9d
subrutm <subrutm@gmail.com>
2015-05-08 01:33:46 +0900

    Sixth Commit
    エミュレータ for STB　V1.0.0で動作するように修正
    チャンネル一覧を出力できるように改良

--- tv_test/app.js
+++ tv_test/app.js
@@ -5,46 +5,59 @@ function debug(message) {
   for(var i = 0; i < message.length; i++) {
     item += message[i] + '\n';
   }
-  newElement.innerHTML = '<textarea name="contents" rows="30" cols="100">' + item + '</textarea>';
+  newElement.innerHTML = '<h1>Support Source Type List:</h1><textarea name="contents" rows=' + message.length +'>' + item + '</textarea>';
   var parent_object = document.getElementById("debug");
   parent_object.appendChild(newElement);
 }
 
-// Retrieve all the available TV tuners.
-navigator.tv.getTuners().then(function onsuccess(tuners) {
-  // Just use the first TV tuner.
-  var tuner = tuners[0];
-  
-  //リンクして再生
-  var video = document.getElementById("video-player");
-  
-  //エミュレータ環境
-  video.src = tuner.stream;
-  video.play();
-  
-  //本番環境
-//  video.srcObject = tuner.stream;
-
-/*
-  // Set the 'oncurrentsourcechanged' event handler.
-  tuner.oncurrentsourcechanged = function oncurrentsourcechanged(event) {
-    alert("The current TV source has changed.");
-  };
-*/
-  // Get the supported TV source types for the TV tuner. 
-  var sourceTypes = tuner.getSupportedSourceTypes();
-  if(sourceTypes.length > 0) {
-    debug(sourceTypes);
+//デバッグ用HTMLを出力
+function debugChannels(message) {
+  var newElement = document.createElement("div");
+  var item = "";
+  for(var i = 0; i < message.length; i++) {
+    item += message[i].name + '\n';
   }
-/*
-  // Just use the first TV source type.
-  tuner.setCurrentSource(sourceTypes[0]).then(function onsuccess() {
-    alert("Succeeded to set the TV source.");
-  }, function onerror(error) {
-    alert(error);
-  });
-*/
-}, function onerror(error) {
-  alert(error);
-});
+  newElement.innerHTML = '<h1>Channel List:</h1><textarea name="contents" rows=' + message.length +'>' + item + '</textarea>';
+  var parent_object = document.getElementById("debug");
+  parent_object.appendChild(newElement);
+}
 
+window.onload = function() {
+  var tv = window.navigator.tv;
+  video = document.getElementById('video-player');
+  
+  if (!tv) {
+    alert ('failed to get tv. check permission.');
+    return;
+  }
+  
+  tv.getTuners().then (function onsuccess(tuners) {
+    if (tuners.length == 0) {
+      alert ('getTuners() fail.');
+      return;
+    }
+    
+    // Get the supported TV source types for the TV tuner. 
+    var sourceTypes = tuners[0].getSupportedSourceTypes();
+    if(sourceTypes.length > 0) {
+      debug(sourceTypes);
+    }
+    
+    tuners[0].setCurrentSource (sourceTypes[0]).then(function onsuccess() {
+      video.mozSrcObject = tuners[0].stream;  // for STB 
+      
+      currentSource = tuners[0].currentSource;
+      currentSource.getChannels().then(function onsuccess(channels) {
+        if(channels.length > 0) {
+          debugChannels(channels);
+        }
+      }, function onerror(error) {
+        alert ('getChannels() error');
+      });
+    }, function onerror(error) {
+      alert ('setCurrentSource() error');
+    });
+  }, function onerror(error) {
+    alert ('getTuners() error.');
+  });
+};
